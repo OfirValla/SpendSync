6d64c4a514a021b113b4c2fdc1655c88c46761a2
Ofir Valla <ofirvalla50@gmail.com>
2024-02-24 00:32:11 +0200

    Adding loader component

--- spendsync/src/App.tsx
+++ spendsync/src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { FC } from 'react';
+//import reactLogo from './assets/react.svg';
+//import viteLogo from '/vite.svg';
+
+import Loader from './components/atoms/Loader';
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from './firebase';
+
 import './App.css';
 
-function App() {
-    const [count, setCount] = useState(0);
+//const ComponentSelector: { [id: string]: FC } = {
+//    'loading': Loader,
+//    'authed': Authed,
+//    'not-authed': Login,
+//    'error': Error
+//};
 
-    return (
-        <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
-    );
+const App: FC = () => {
+    const [user, loading, error] = useAuthState(auth);
+
+    let state = 'loading';
+    if (user) state = 'authed';
+    if (!user && !loading) state = 'not-authed';
+    if (error) state = 'error';
+
+    console.log({ user, loading, state });
+    //const Component = ComponentSelector[state];
+    //return (
+    //    <Component />
+    //);
+
+    return <Loader />;
 }
 
 export default App;
---
+++ spendsync/src/components/atoms/Loader.tsx
@@ -0,0 +1,38 @@
+import { FC } from 'react';
+import * as stylex from '@stylexjs/stylex';
+
+import './Loader.css';
+
+const styles = stylex.create({
+    svg: {
+        width: '20vw',
+        height: '20vw',
+        animation: 'spin 1s linear infinite'
+    },
+    circle: {
+        fill: 'none',
+        stroke: 'url(#linear)',
+        strokeWidth: '3px',
+        strokeLinecap: 'round',
+        animation: 'dash 2.5s ease-in-out infinite'
+    }
+});
+
+const Loader: FC = () => (
+    <svg viewBox="0 0 50 50" {...stylex.props(styles.svg)}>
+        <defs>
+            <linearGradient id="linear" x1="0%" y1="0%" x2="100%" y2="0%">
+                <stop offset="0%" stopColor="#007bff" />
+                <stop offset="100%" stopColor="#00aa55" />
+            </linearGradient>
+        </defs>
+        <circle
+            cx="25"
+            cy="25"
+            r="20"
+            {...stylex.props(styles.circle)}
+        />
+    </svg>
+);
+
+export default Loader;
