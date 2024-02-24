e1c6cd3748819abe664b829c3e2f49b3dd41de1a
Ofir Valla <ofirvalla50@gmail.com>
2024-02-24 15:08:17 +0200

    Placing the loader above everything

--- spendsync/src/components/atoms/Loader/Loader.tsx
+++ spendsync/src/components/atoms/Loader/Loader.tsx
@@ -4,6 +4,16 @@ import * as stylex from '@stylexjs/stylex';
 import './Loader.css';
 
 const styles = stylex.create({
+    container: {
+        display: 'grid',
+        justifyContent: 'center',
+        alignContent: 'center',
+        height: '100vh',
+        width: '100vw',
+        position: 'absolute',
+        top: '0',
+        right: '0'
+    },
     svg: {
         width: '20vw',
         height: '20vw',
@@ -19,20 +29,22 @@ const styles = stylex.create({
 });
 
 const Loader: FC = () => (
-    <svg viewBox="0 0 50 50" {...stylex.props(styles.svg)}>
-        <defs>
-            <linearGradient id="linear" x1="0%" y1="0%" x2="100%" y2="0%">
-                <stop offset="0%" stopColor="#007bff" />
-                <stop offset="100%" stopColor="#00aa55" />
-            </linearGradient>
-        </defs>
-        <circle
-            cx="25"
-            cy="25"
-            r="20"
-            {...stylex.props(styles.circle)}
-        />
-    </svg>
+    <div {...stylex.props(styles.container)}>
+        <svg viewBox="0 0 50 50" {...stylex.props(styles.svg)}>
+            <defs>
+                <linearGradient id="linear" x1="0%" y1="0%" x2="100%" y2="0%">
+                    <stop offset="0%" stopColor="#007bff" />
+                    <stop offset="100%" stopColor="#00aa55" />
+                </linearGradient>
+            </defs>
+            <circle
+                cx="25"
+                cy="25"
+                r="20"
+                {...stylex.props(styles.circle)}
+            />
+        </svg>
+    </div>
 );
 
 export default Loader;
