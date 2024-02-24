fdcf585dcd884df54dc0b57ae1fbc82950ed201d
Ofir Valla <ofirvalla50@gmail.com>
2024-02-24 16:19:41 +0200

    Joining the current user to all of the groups that it was invited to

--- spendsync/src/pages/Authed/Groups/Groups.tsx
+++ spendsync/src/pages/Authed/Groups/Groups.tsx
@@ -1,10 +1,58 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { ref, set, remove, onValue } from "firebase/database";
+
 
 import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from '../../../firebase';
+import { auth, db } from '../../../firebase';
 
 const Groups: FC = () => {
-    const [user,, ] = useAuthState(auth);
+    const [user, ,] = useAuthState(auth);
+
+    const handleNewInvites = () => {
+        console.log("Listening to invites");
+
+        return onValue(ref(db, `invites/${user!.email!.replace('.', ',')}`),
+            snapshot => {
+                const data = snapshot.val();
+                if (!data) return;
+
+                const invites = Object.keys(data);
+                for (const groupId of invites) {
+                    console.groupCollapsed('Adding user to group');
+                    console.log(`Group id: ${groupId}`);
+
+                    // Get the invite and write it to the users groups
+                    console.log('Adding group to users groups');
+                    set(ref(db, `users/${user!.uid}/groups/${groupId}`), true);
+
+                    // Remove the invite from the list of invites
+                    console.log('Removing invite for group');
+                    remove(ref(db, `invites/${user!.email!.replace('.', ',')}/${groupId}`));
+
+                    console.groupEnd();
+                }
+            },
+            error => console.error(error)
+        );
+    }
+
+    const handleUserGroups = () => {
+        return onValue(ref(db, `users/${user!.uid}/groups`),
+            snapshot => console.log(snapshot.val()),
+            error => console.error(error)
+        );
+    }
+
+    useEffect(() => {
+        const unsubscribeNewInvites = handleNewInvites();        
+        const unsubscribeUserGroups = handleUserGroups();
+        
+        return () => {
+            unsubscribeNewInvites();
+            unsubscribeUserGroups();
+        }
+    }, []);
+
     return (
         <>
             <div>Groups</div>
