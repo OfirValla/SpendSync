359dd0612255081f07e5acdce9e5d114f4006474
Ofir Valla <ofirvalla50@gmail.com>
2024-02-25 21:45:59 +0200

    Adding initial code for creating new groups

--- spendsync/src/pages/Authed/AddNewGroup/AddNewGroup.tsx
+++ spendsync/src/pages/Authed/AddNewGroup/AddNewGroup.tsx
@@ -1,10 +1,40 @@
 import { FC } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { ref, set, update } from "firebase/database";
+import { v4 as uuidv4 } from 'uuid';
 
-import { db } from '../../../firebase';
+import { auth, db } from '../../../firebase';
 
 const AddNewGroup: FC = () => {
+    const [user, ,] = useAuthState(auth);
+
+    const addNewGroup = async () => {
+        const groupId = uuidv4();
+        await Promise.all([
+            set(
+                ref(db, `groups/${groupId}`),
+                {
+                    members: {
+                        [user!.uid]: true
+                    },
+                    name: `Test Group - ${groupId}`,
+                    owed: {
+                        [user!.uid]: 0
+                    }
+                }
+            ),
+
+            update(
+                ref(db, `users/${user!.uid}/groups`),
+                {
+                    [groupId]: true
+                }
+            )
+        ]);
+    }
+
     return (
-        <div>New Group</div>
+        <button onClick={addNewGroup}>New Group</button>
     )
 };
 
