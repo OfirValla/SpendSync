1da99de431d4761fbf0bf79e01e64d0ed9954efe
Ofir Valla <ofirvalla50@gmail.com>
2024-02-27 08:45:03 +0200

    Figuring out the pagination

--- spendsync/src/pages/Authed/ViewGroup/ViewGroup.tsx
+++ spendsync/src/pages/Authed/ViewGroup/ViewGroup.tsx
@@ -1,11 +1,28 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { ref, query, get, limitToLast, endBefore, orderByKey } from 'firebase/database';
 
 import { db } from '../../../firebase';
 
+type Activity = {
+    title: string;
+    amount: number;
+    currency: string;
+    createdAt: string;
+    paidBy: string;
+    split: { [key: string]: number; };
+};
+
 const ViewGroup: FC = () => {
     const { groupId } = useParams();
-    
+
+    useEffect(() => {
+        get(query(ref(db, `groups/${groupId}/activity`), orderByKey(), endBefore('-NrdHaDPwiL4Mj1hv-HM'), limitToLast(3))).then(res => {
+            const data = res.val()
+            console.log(res, data);
+        })
+            
+    }, [])
     
     return (
         <div>View Group {groupId}</div>
