0014587d44f81e470d71174a9a5f540189ef71b8
Ofir Valla <ofirvalla50@gmail.com>
2024-03-01 16:49:36 +0200

    Fixing the inifinite scroll + adding onChildAdded events

--- spendsync/src/pages/Authed/Groups/Groups.tsx
+++ spendsync/src/pages/Authed/Groups/Groups.tsx
@@ -1,10 +1,11 @@
 import { FC, useEffect, useState } from 'react';
-import { ref, onValue, get, onChildAdded, query, onChildRemoved, onChildChanged } from "firebase/database";
-
+import { ref, get, onChildAdded, query, onChildRemoved, DataSnapshot, orderByKey, endBefore, limitToLast, Query, startAfter } from "firebase/database";
+import useInfiniteScroll from 'react-infinite-scroll-hook';
 import { useAuthState } from "react-firebase-hooks/auth";
+
 import { auth, db } from '../../../firebase';
 
-type GroupInformation = {
+type Group = {
     id: string;
     name: string;
     owed: { [key: string]: number; };
@@ -12,75 +13,138 @@ type GroupInformation = {
 
 const Groups: FC = () => {
     const [user, ,] = useAuthState(auth);
-    const [groups, setGroups] = useState<GroupInformation[]>([]);
-    const [renderedGroupIds, setRenderedGroupIds] = useState<string[]>([]);
-    
-    const handleUserGroups = () => {
-        return onValue(ref(db, `users/${user!.uid}/groups`),
-            async (snapshot) => {
-                const data = snapshot.val();
-                if (!data) return;
+    const [groups, setGroups] = useState<Group[]>([]);    
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [hasNextPage, setHasNextPage] = useState<boolean>(true);
+    const [firstItem, setFirstItem] = useState<string | null | undefined>(null);
 
-                const newGroups: GroupInformation[] = [];
-                const groupIds = Object.keys(data);
-                for (const groupId of groupIds) {
-                    if (renderedGroupIds.includes(groupId)) continue;
+    const fetchData = async () => {
+        console.groupCollapsed("Fetching Groups");
 
-                    const [nameData, owedData] = await Promise.all([
-                        get(ref(db, `groups/${groupId}/name`)),
-                        get(ref(db, `groups/${groupId}/owed`))
-                    ]);
-                    newGroups.push({ id: groupId, name: nameData.val(), owed: owedData.val() })
-                }
-                setRenderedGroupIds(prev => [...prev, ...newGroups.map(g => g.id)]);
-                setGroups(prev => [...prev, ...newGroups]);
-            },
-            error => console.error(error)
-        );
-    }
-    
-    useEffect(() => {
-        const onChildAddedUnsubscribe = onChildAdded(query(ref(db, `users/${user!.uid}/groups`)), async (data) => {
-            console.groupCollapsed("Adding Group");
-            console.log(`Id: ${data.key}`);
+        if (isLoading) {
+            console.log("Already loading");
+            console.groupEnd();
+            return;
+        }
+        setIsLoading(true);
+
+        let result: DataSnapshot;
+        if (groups.length !== 0)
+            result = await get(query(ref(db, `users/${user!.uid}/groups`), orderByKey(), endBefore(groups!.at(-1)!.id), limitToLast(10)));
+        else {
+            result = await get(query(ref(db, `users/${user!.uid}/groups`), orderByKey(), limitToLast(10)));
+            setFirstItem(Object.keys(result.val() || {}).at(-1));
+        }
+
+        const data: { [key: string]: boolean; } = result.val();
+        if (!data) {
+            console.log("No more groups");
+            setHasNextPage(false);
+            console.groupEnd();
+            return;
+        }
+
+        const newGroups: Group[] = [];
+        const groupIds = Object.keys(data).reverse();
+        for (const groupId of groupIds) {
+            if (groups.some(group => group.id === groupId)) continue;
 
             const [nameData, owedData] = await Promise.all([
-                get(ref(db, `groups/${data.key}/name`)),
-                get(ref(db, `groups/${data.key}/owed`))
+                get(ref(db, `groups/${groupId}/name`)),
+                get(ref(db, `groups/${groupId}/owed`))
             ]);
+            newGroups.push({ id: groupId, name: nameData.val(), owed: owedData.val() });
+        }
+        setGroups(prev => [...prev, ...newGroups]);
 
-            setGroups(prev => {
-                if (prev.some(e => e.id === data.key))
-                    return prev;
-                const newGroup: GroupInformation = { id: data.key, name: nameData.val(), owed: owedData.val() };
-                return [...prev, newGroup];
-            });
+        console.log(`Found ${newGroups.length} new groups`);
+        console.groupEnd();
 
-            console.log(`Title: ${nameData.val()}`);
-            console.groupEnd();
+        setIsLoading(false);
+    };
 
-        });
+    const [sentryRef, { rootRef }] = useInfiniteScroll({
+        loading: isLoading,
+        hasNextPage,
+        onLoadMore: fetchData,
+        // `rootMargin` is passed to `IntersectionObserver`.
+        // We can use it to trigger 'onLoadMore' when the sentry comes near to become
+        // visible, instead of becoming fully visible on the screen.
+        rootMargin: '0px 0px 400px 0px',
+    });
 
+    const onChildAddedCallback = async (data: DataSnapshot) => {
+        console.groupCollapsed("Group Added");
+        console.log(`Id: ${data.key}`);
+
+        const [nameData, owedData] = await Promise.all([
+            get(ref(db, `groups/${data.key}/name`)),
+            get(ref(db, `groups/${data.key}/owed`))
+        ]);
+        const newGroup: Group = { id: data.key, name: nameData.val(), owed: owedData.val() };
+
+        console.log(`Title: ${newGroup.name}`);
+        console.groupEnd();
+
+        setGroups(prev => [newGroup, ...prev]);
+    };
+
+    useEffect(() => {
+        let groupAddedQuery: Query | null = null;
+        if ((firstItem === null || firstItem === undefined) && !hasNextPage)
+            groupAddedQuery = query(ref(db, `users/${user!.uid}/groups`));
+
+        if (firstItem !== null && firstItem !== undefined)
+            groupAddedQuery = query(ref(db, `users/${user!.uid}/groups`), orderByKey(), startAfter(firstItem));
+
+        if (!groupAddedQuery) return;
+
+        return onChildAdded(
+            groupAddedQuery,
+            onChildAddedCallback
+        );
+    }, [firstItem, hasNextPage]);
+
+    useEffect(() => {
         const onChildRemovedUnsubscribe = onChildRemoved(ref(db, `users/${user!.uid}/groups`), (data) => {
             console.groupCollapsed("Removing Group");
             console.log(`Id: ${data.key}`);
-            console.log(`Title: ${data.val().title}`);
             console.groupEnd();
-
             setGroups(prev => prev.filter(group => group.id !== data.key));
         });
-        
+
+        //const onChildChangedUnsubscribe = onChildChanged(ref(db, `users/${user!.uid}/groups`), (data) => {
+        //    console.groupCollapsed("Activity Changed");
+        //    console.log(`Id: ${data.key}`);
+        //    console.log(`New Data: ${data.val()}`);
+        //    console.groupEnd();
+
+        //    setActivities(prev => {
+        //        const itemIdx = prev.findIndex(item => item.id === data.key);
+        //        if (itemIdx === -1) return prev;
+
+        //        return [
+        //            ...prev.slice(0, itemIdx),
+        //            { id: data.key, ...data.val() },
+        //            ...prev.slice(itemIdx + 1)
+        //        ];
+        //    });
+        //});
+
         return () => {
-            onChildAddedUnsubscribe();
             onChildRemovedUnsubscribe();
+            //onChildChangedUnsubscribe();
         };
     }, []);
-
+    
     return (
         <>
             <div>Groups</div>
             <div>{JSON.stringify({ uid: user!.uid, email: user!.email, displayName: user!.displayName, photoUrl: user!.photoURL })}</div>
-            <div>{groups.map(group => <div key={group.id}>{JSON.stringify(group)}</div>)}</div>
+            <div ref={rootRef}>
+                {groups.map(group => <div key={group.id}>{JSON.stringify(group)}</div>)}
+                <div ref={sentryRef}></div>
+            </div>
         </>
     )
 };
--- spendsync/src/pages/Authed/ViewGroup/ViewGroup.tsx
+++ spendsync/src/pages/Authed/ViewGroup/ViewGroup.tsx
@@ -1,6 +1,7 @@
 import { FC, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { ref, query, onChildAdded, onChildRemoved, onChildChanged } from 'firebase/database';
+import useInfiniteScroll from 'react-infinite-scroll-hook';
+import { ref, query, get, limitToLast, endBefore, orderByKey, DataSnapshot, onChildAdded, startAfter, onChildRemoved, onChildChanged, Query } from 'firebase/database';
 
 import { db } from '../../../firebase';
 
@@ -18,30 +19,88 @@ type Activity = ActivityDTO & { id: string | null; };
 const ViewGroup: FC = () => {
     const { groupId } = useParams();
     const [activities, setActivities] = useState<Activity[]>([]);
-    
-    useEffect(() => {
-        const onChildAddedUnsubscribe = onChildAdded(query(ref(db, `groups/${groupId}/activity`)), (data) => {
-            const result: ActivityDTO = data.val();
-            const newActivity: Activity = { id: data.key, ...result };
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [hasNextPage, setHasNextPage] = useState<boolean>(true);
+    const [firstItem, setFirstItem] = useState<string | null | undefined>(null);
 
-            console.groupCollapsed("Adding Activity");
-            console.log(`Id: ${newActivity.id}`);
-            console.log(`Title: ${newActivity.title}`);
+    const fetchData = async () => {
+        console.groupCollapsed("Fetching Activities");
+
+        if (isLoading) {
+            console.log("Already loading");
             console.groupEnd();
+            return;
+        }
+        setIsLoading(true);
 
-            setActivities(prev => {
-                if (prev.some(e => e.id === newActivity.id))
-                    return prev;
-                return [newActivity, ...prev];
-            });
-        });
+        let result: DataSnapshot;
+        if (activities.length !== 0)
+            result = await get(query(ref(db, `groups/${groupId}/activity`), orderByKey(), endBefore(activities!.at(-1)!.id), limitToLast(10)));
+        else {
+            result = await get(query(ref(db, `groups/${groupId}/activity`), orderByKey(), limitToLast(10)));
+            setFirstItem(Object.keys(result.val() || {}).at(-1));
+        }
 
+        const data: { [key: string]: ActivityDTO; } = result.val();
+        if (!data) {
+            console.log("No more activities");
+            setHasNextPage(false);
+            console.groupEnd();
+            return;
+        }
+
+        const newActivities = (Object.keys(data) || []).map(id => { return { id, ...data[id] }; }).reverse();
+        setActivities(prev => [...prev, ...newActivities]);
+
+        console.log(`Found ${newActivities.length} new activities`);
+        console.groupEnd();
+
+        setIsLoading(false);
+    };
+
+    const [sentryRef, { rootRef }] = useInfiniteScroll({
+        loading: isLoading,
+        hasNextPage,
+        onLoadMore: fetchData,
+        // `rootMargin` is passed to `IntersectionObserver`.
+        // We can use it to trigger 'onLoadMore' when the sentry comes near to become
+        // visible, instead of becoming fully visible on the screen.
+        rootMargin: '0px 0px 400px 0px',
+    });
+
+    const onChildAddedCallback = (data: DataSnapshot) => {
+        console.groupCollapsed("Activity Added");
+        console.log(`Id: ${data.key}`);
+        const result: ActivityDTO = data.val();
+        const newActivity: Activity = { id: data.key, ...result };
+        console.log(`Title: ${newActivity.title}`);
+        console.groupEnd();
+
+        setActivities(prev => [newActivity, ...prev]);
+    }
+
+    useEffect(() => {
+        let activityAddedQuery: Query | null = null;
+        if ((firstItem === null || firstItem === undefined) && !hasNextPage) 
+            activityAddedQuery = query(ref(db, `groups/${groupId}/activity`));
+
+        if (firstItem !== null && firstItem !== undefined)
+            activityAddedQuery = query(ref(db, `groups/${groupId}/activity`), orderByKey(), startAfter(firstItem));
+
+        if (!activityAddedQuery) return;
+
+        return onChildAdded(
+            activityAddedQuery,
+            onChildAddedCallback
+        );
+    }, [firstItem, hasNextPage]);
+
+    useEffect(() => {
         const onChildRemovedUnsubscribe = onChildRemoved(ref(db, `groups/${groupId}/activity`), (data) => {
             console.groupCollapsed("Removing Activity");
             console.log(`Id: ${data.key}`);
             console.log(`Title: ${data.val().title}`);
             console.groupEnd();
-
             setActivities(prev => prev.filter(activity => activity.id !== data.key));
         });
 
@@ -64,7 +123,6 @@ const ViewGroup: FC = () => {
         });
 
         return () => {
-            onChildAddedUnsubscribe();
             onChildRemovedUnsubscribe();
             onChildChangedUnsubscribe();
         };
@@ -73,15 +131,17 @@ const ViewGroup: FC = () => {
     return (
         <>
             <div>View Group {groupId}</div>
-            <div >
+            <div ref={rootRef}>
                 {
                     activities.map((activity, index) => {
                         return <div key={index}>{activity.id} - {activity.title}</div>;
                     })
                 }
+
+                <div ref={sentryRef}></div>
             </div>
         </>
-    )
+    );
 };
 
 export default ViewGroup;
