041b6e8b9f338a972f1f16d94ba712895a670893
Ofir Valla <ofirvalla50@gmail.com>
2024-03-02 14:38:30 +0200

    Disable console logs of any kind in production

--- spendsync/src/main.tsx
+++ spendsync/src/main.tsx
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Disable logs in production
+if (process.env.NODE_ENV === 'production') {
+    console.group = () => { };
+    console.groupEnd = () => { };
+    console.groupCollapsed = () => { };
+    console.log = () => { };
+    console.error = () => { };
+    console.debug = () => { };
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
