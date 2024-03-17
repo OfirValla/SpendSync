f509348ebf5c6c7dbadfdf71e5c89a3080fbba79
Ofir Valla <ofirvalla50@gmail.com>
2024-03-18 00:50:57 +0200

    Changing to arrow function

--- spendsync/src/hooks/useDocumentTitle.tsx
+++ spendsync/src/hooks/useDocumentTitle.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 
-export function useDocumentTitle(title: string) {
+export const useDocumentTitle = (title: string) => {
     useEffect(() => {
         const prevTitle = document.title;
         document.title = title;
