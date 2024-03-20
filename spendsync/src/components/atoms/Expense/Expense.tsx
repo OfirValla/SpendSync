74441910b97df73f557f30bfd27438fc57ce7511
Ofir Valla <ofirvalla50@gmail.com>
2024-03-21 00:26:24 +0200

    Adding the icon

--- spendsync/src/components/atoms/Expense/Expense.tsx
+++ spendsync/src/components/atoms/Expense/Expense.tsx
@@ -2,10 +2,10 @@ import { FC } from 'react';
 import { Expense as ExpenseType } from '../../../types/Expense';
 import { Currency } from '../../../utils/Currency';
 
-const Expense: FC<ExpenseType> = ({ id, title, amount, currency, createdAt, paidBy, split }) => {
+const Expense: FC<ExpenseType> = ({ id, title, icon, amount, currency, createdAt, paidBy, split }) => {
     return (
         <div style={{ backgroundColor: '#9d9d9d42', border: '1px solid white', borderRadius: '15px', padding: '10px' }}>
-            <b>{id}</b> - {title} - {amount}{Currency[currency].symbol} - {`${new Date(createdAt).getDate()}/${new Date(createdAt).getMonth() + 1}/${new Date(createdAt).getFullYear()}`} - {paidBy} - {JSON.stringify(split)}
+            <b>{id}</b> - {icon} - {title} - {amount}{Currency[currency].symbol} - {`${new Date(createdAt).getDate()}/${new Date(createdAt).getMonth() + 1}/${new Date(createdAt).getFullYear()}`} - {paidBy} - {JSON.stringify(split)}
         </div>
     );
 };
